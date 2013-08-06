0d9bd5e84f27da87cc167c8a319301c61ceb747f
Siddhu Arevindh <arevindh@gmail.com>
2013-08-06 19:45:29 +0530

    UI Improvement

--- wp.php
+++ wp.php
@@ -1,14 +1,15 @@
 <?php
 /* Wordpress Easy Instaler */
-/* Version 0.8 beta */
+/* Version 0.8b */
 /* 
+
 Coded By Siddhu arevindh
-http://lab.arevindh.com/
+http://lab.arevindh.com 
 Email : siddhu@arevindh.com
+
 */
 
-
-/* save files from wordpress.org */
+/* Get files from wordpress.org */
 
 
 $root = realpath(dirname(__FILE__));
@@ -18,12 +19,20 @@ $page=$_GET['step'];
 $source=$root."/wordpress/";
 $destination=$root."/";
 
+?>
+
+<html>
+<title> WordPress Installer By Siddhu Arevindh  <?php if (isset($_GET['step'])) { echo " |  Step: ".$page; }  ?> </title>
+<meta name="robots" content="noindex,nofollow">
+<body>
+<?php
+
 if (isset($_GET['step'])) { }
 else 
 
 {     
          echo "<h2> Wordpress Easy Install </h2> <br />";
-	 echo "<a href='wp.php?step=1'> Proceed with Install </a>";
+	 echo "<b><br /><a href='wp.php?step=1'> Proceed with Install </a></b>";
 	 echo " <br /> <br /> <br /> Script By <a href='http://www.arevindh.com/'> Siddhu Arevindh  </a>";
 }
 
@@ -35,7 +44,7 @@ if ($page==1)
 
 	/* Unzip File */
 	echo "File Downoad Complete";
-	echo "<b> <a href='wp.php?step=2'> Next Step (2) </a> </b>";
+	echo "<b> <br /> <a href='wp.php?step=2'> Next Step (2) </a> </b>";
 	  echo " <br /> <br /> <br /> Script By  <a href='http://www.arevindh.com/'> Siddhu Arevindh  </a>";
 
 }
@@ -48,7 +57,7 @@ else if ($page==2)
 			$zip->extractTo($root); 
 			$zip->close(); 
 			echo 'File Unzip Complete !';
-			echo "<a href='wp.php?step=3'> Next Step (3) </a>";		
+			echo "<b><br /><a href='wp.php?step=3'> Next Step (3) </a></b>";		
 			echo " <br /> <br /> <br /> Script By  <a href='http://www.arevindh.com/'> Siddhu Arevindh  </a>";			
 		} 
     
@@ -63,7 +72,7 @@ else if ($page==3)
 {
   recurse_copy($source,$destination);
   echo "Success <br /> Click Next to Delete Temp File and this script";
-  echo " <br /><a href='wp.php?step=4'> Next Step (4) </a>";
+  echo "<b><br /><a href='wp.php?step=4'> Next Step (4) </a></b>";
   echo " <br /> <br /> <br /> Script By  <a href='http://www.arevindh.com/'> Siddhu Arevindh  </a>";
 }
 
@@ -80,7 +89,7 @@ echo "<br /> Folder Removed ";
 unlink(__FILE__);
 echo "<br /> Instal Script Removed ";
 
-echo " <br />Done !!!!  <br /><a href='index.php'> Proceed with Database install </a>";
+echo " <br />Done !!!! <br />  <br /><a href='index.php'> Proceed with Database install </a>";
 
 echo " <br /> <br /> <br />Script By  <a href='http://www.arevindh.com/'> Siddhu Arevindh  </a>";
 }
@@ -116,3 +125,6 @@ function recurse_copy($src,$dst)
 
 
 ?>
+
+</body>
+</html>
